515ccf219301410dd2d66632731260b8cc139722
Timothy Mubiru <mubstimor@gmail.com>
2019-11-08 16:10:00 +0300

    complete code functionality

--- app/src/main/java/com/example/android/codelabs/navigation/HomeFragment.kt
+++ app/src/main/java/com/example/android/codelabs/navigation/HomeFragment.kt
@@ -53,9 +53,16 @@ class HomeFragment : Fragment() {
             findNavController().navigate(R.id.flow_step_one_dest, null, options)
         }
 
-        view.findViewById<Button>(R.id.navigate_action_button)?.setOnClickListener(
-                Navigation.createNavigateOnClickListener(R.id.next_action, null)
-        )
+//        view.findViewById<Button>(R.id.navigate_action_button)?.setOnClickListener(
+////                Navigation.createNavigateOnClickListener(R.id.next_action, null)
+////        )
+
+        // Note the usage of curly braces since we are defining the click listener lambda
+        view.findViewById<Button>(R.id.navigate_action_button)?.setOnClickListener{
+            val flowStepNumberArg = 1
+            val action = HomeFragmentDirections.nextAction(flowStepNumberArg)
+            findNavController().navigate(action)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
